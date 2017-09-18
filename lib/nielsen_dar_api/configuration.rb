f5e4f2fa26dd94380a14eeb4957004284149d4da
Mauro Berlanda <mauro.berlanda@mecglobal.com>
2017-09-18 13:06:22 +0200

    Tested some validation on configuration

--- lib/nielsen_dar_api/configuration.rb
+++ lib/nielsen_dar_api/configuration.rb
@@ -1,12 +1,28 @@
 module NielsenDarApi
   class Configuration
-    attr_accessor :auth_url, :base_url, :country_code, :date_format
+    attr_accessor :auth_url, :base_url, :country_code, :date_format,
+                  :username, :password, :basic_token, :grant_type
 
     def initialize
       @auth_url = 'https://api.developer.nielsen.com/watch/oauth/token'
       @base_url = 'https://api.developer.nielsen.com/watch/dar'
       @country_code = 'IT'
       @date_format = '%m/%d/%Y'
+      @username = nil
+      @password = nil
+      @basic_token = nil
+      @grant_type = 'password'
+    end
+
+    def validate_credentials!
+      [:username, :password, :basic_token, :grant_type].each do |m|
+        send(m) || required_field(m)
+      end
+      true
+    end
+
+    def required_field(name)
+      raise NoMethodError, "Please define #{name} in NielsenDarApi configuration"
     end
   end
 end
--- spec/nielsen_dar_api_spec.rb
+++ spec/nielsen_dar_api_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe NielsenDarApi do
     expect(NielsenDarApi::VERSION).not_to be nil
   end
 
-  context "should have some default configuration" do
+  context "has some default configuration" do
     before(:all) do
       NielsenDarApi.configure {}
     end
@@ -15,6 +15,28 @@ RSpec.describe NielsenDarApi do
     it { should respond_to(:base_url) }
     it { should respond_to(:country_code) }
     it { should respond_to(:date_format) }
+    it { should respond_to(:username) }
+    it { should respond_to(:password) }
+    it { should respond_to(:basic_token) }
+    it { should respond_to(:grant_type) }
+
+    it "should fail validate_credentials!" do
+      expect {
+        NielsenDarApi.configuration.validate_credentials!
+      }.to raise_error( NoMethodError, /Please define/)
+    end
   end
 
+  context "requires some configuration" do
+    before do
+      NielsenDarApi.configure do |config|
+        config.username = 'someone@example.com'
+        config.password = 'password'
+        config.basic_token = 'Basic c29tZW9uZUBleGFtcGxlLmNvbTpwYXNzd29yZA=='
+      end
+    end
+    it "should pass validate_credentials! with username, password and basic_token" do
+      expect(NielsenDarApi.configuration.validate_credentials!).to be(true)
+    end
+  end
 end
